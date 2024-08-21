2fefc7e44e5ec7bf5674ea3d1ba236ab664feecf
Victor Rodrigues <victor.rorigues.ext@infracommerce.com>
2024-08-21 16:39:42 -0300

    Added all civ flags

--- src/assets/images/flags/index.ts
+++ src/assets/images/flags/index.ts
@@ -1,7 +1,35 @@
-import byzantines from "./byzantines.webp";
-import hre from "./hre.webp";
-import french from "./french.webp";
-import delhi from "./delhi.webp";
-import jeannedarc from "./jeannedarc.webp";
+import abbasid from "./abbasid.webp";
+import ayyubids from "./ayyubids.abbasid.webp";
+import byzantines from "./byzantines.abbasid.webp";
+import chinese from "./chinese.abbasid.webp";
+import delhi from "./delhi.abbasid.webp";
+import english from "./english.abbasid.webp";
+import french from "./french.abbasid.webp";
+import hre from "./hre.abbasid.webp";
+import japanese from "./japanese.abbasid.webp";
+import jeannedarc from "./jeannedarc.abbasid.webp";
+import malians from "./malians.abbasid.webp";
+import mongols from "./mongols.abbasid.webp";
+import orderofthedragon from "./orderofthedragon.abbasid.webp";
+import ottomans from "./ottomans.abbasid.webp";
+import rus from "./rus.abbasid.webp";
+import zhuxi from "./zhuxi.abbasid.webp";
 
-export { byzantines, hre, french, delhi, jeannedarc };
+export {
+  abbasid,
+  ayyubids,
+  byzantines,
+  chinese,
+  delhi,
+  english,
+  french,
+  hre,
+  japanese,
+  jeannedarc,
+  malians,
+  mongols,
+  orderofthedragon,
+  ottomans,
+  rus,
+  zhuxi,
+};
--- src/infra/api/requests/list-civs.ts
+++ src/infra/api/requests/list-civs.ts
@@ -1,11 +1,19 @@
 import { CivilizationsEnum } from "@domain/enums/civs";
+import * as flags from "@assets/images/flags";
 
 type Params = {
   name?: string;
 };
 
-export function listCivs(params?: Params): CivilizationsEnum[] {
-  return Object.values(CivilizationsEnum).filter((civ) =>
-    params?.name ? civ.includes(params.name.toLowerCase()) : true
-  );
+type Response = {
+  name: CivilizationsEnum;
+  flag: string;
+}[];
+
+export function listCivs(params?: Params): Response {
+  return Object.values(CivilizationsEnum)
+    .filter((civ) =>
+      params?.name ? civ.includes(params.name.toLowerCase()) : true
+    )
+    .map((civName) => ({ name: civName, flag: flags[civName] }));
 }
